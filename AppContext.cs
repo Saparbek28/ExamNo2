ba2a6cc059187a45a156f2412fd4cc01a3cb0757
Saparbek Bainazar <Saparbek28@gmail.com>
2019-04-26 16:45:03 +0600

    first commit

---
+++ AppContext.cs
@@ -0,0 +1,34 @@
+﻿namespace CRUD
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class AppContext : DbContext
+    {
+        // Контекст настроен для использования строки подключения "AppContext" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "CRUD.AppContext" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "AppContext" 
+        // в файле конфигурации приложения.
+        public AppContext()
+            : base("name=AppContext")
+        {
+        }
+        public DbSet<Country>Countries { get; set; }
+        public DbSet<Street>Streets { get; set; }
+        public DbSet<City>Cities { get; set; }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var country = new Country
+            //{
+            //    Name = "Китай"
+            //};
+
+            //var city = new City
+            //{
+            //    Name = "Пекин"
+            //};
+
+            //var street = new Street
+            //{
+            //    Name = "Ванфуцзинь"
+            //};
+
+            //using (var context = new AppContext())
+            //{
+            //    context.Countries.Add(country);
+            //    context.Cities.Add(city);
+            //    context.Streets.Add(street);
+            //   // context.SaveChanges();
+            //}
+
+            using (var context = new AppContext())
+            {
+                Street street = context.Streets
+                    .Include(s=>s)
+            }
+
+        }
+    }
+}
